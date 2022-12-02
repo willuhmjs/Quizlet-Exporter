ab84868e7b141b6dafd1290785e46685f2b3ffd9
Tristan F <LeoDog896@hotmail.com>
2022-12-02 17:05:24 -0500

    fmt & fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import parse from "./parser";
-	import docx from "./docx";
+	import parse from './parser';
+	import docx from './docx';
 
 	async function createDocx(file: File) {
 		const parsedData = await parse(file);
@@ -10,21 +10,16 @@
 </script>
 
 <label for="avatar">Upload an HTML file:</label>
-<input
-	accept=".html,.htm,.mhtml"
-	bind:files
-	name="html"
-	type="file"
-/>
+<input accept=".html,.htm,.mhtml" bind:files name="html" type="file" />
 
 {#if files}
 	{#each Array.from(files) as file}
 		${#await createDocx(file)}
-            <p>Loading...</p>
-        {:then buffer}
-            <pre>{buffer}</pre>
+			<p>Loading...</p>
+		{:then buffer}
+			<pre>{buffer}</pre>
 		{:catch error}
 			<pre>{error}</pre>
-        {/await}
+		{/await}
 	{/each}
 {/if}
--- src/routes/docx.ts
+++ src/routes/docx.ts
@@ -1,48 +1,46 @@
-import docx from "docx";
-const { Table, Paragraph, TableCell, TableRow, Document, Packer, TextRun } = docx;
+import { Table, Paragraph, TableCell, TableRow, Document, Packer, TextRun } from 'docx';
 
 export interface Card {
-    term: string;
-    definition: string;
+	term: string;
+	definition: string;
 }
 
 function toRow(card: Card): InstanceType<typeof TableRow> {
-  return new TableRow({
-    children: [
-      new TableCell({
-        width: { size: 100 },
-        children: [
-          new Paragraph({
-            children: [new TextRun({text: card.term, bold: true})],
-          })
-        ]
-      }),
-      new TableCell({
-        children: [
-          new Paragraph({
-            text: card.definition
-          })
-        ]
-      })
-    ]
-  })
+	return new TableRow({
+		children: [
+			new TableCell({
+				width: { size: 100 },
+				children: [
+					new Paragraph({
+						children: [new TextRun({ text: card.term, bold: true })]
+					})
+				]
+			}),
+			new TableCell({
+				children: [
+					new Paragraph({
+						text: card.definition
+					})
+				]
+			})
+		]
+	});
 }
 
 export default async function MakeDoc(cards: Card[]): Promise<Buffer> {
-  let rowArray: InstanceType<typeof TableRow>[] = cards.map(card => toRow(card));
+	let rowArray: TableRow[] = cards.map((card) => toRow(card));
 
-  const table = new Table({
-    rows: rowArray
-  })
-  
-  const doc = new Document({
-    sections: [{
-      children: [
-        table
-      ]
-    }]
-  })
-  
-  return await Packer.toBuffer(doc);
+	const table = new Table({
+		rows: rowArray
+	});
 
+	const doc = new Document({
+		sections: [
+			{
+				children: [table]
+			}
+		]
+	});
+
+	return await Packer.toBuffer(doc);
 }
