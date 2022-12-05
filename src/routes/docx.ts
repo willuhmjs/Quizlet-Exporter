e919a20ebb9afcecc16522e4badef6d50b7cfd9a
William Faircloth <willuhmjs@gmail.com>
2022-12-05 00:54:24 +0000

    make non-docx friendly

--- src/routes/docx.ts
+++ src/routes/docx.ts
@@ -1,11 +1,11 @@
-import { Table, Paragraph, TableCell, TableRow, Document, Packer, TextRun } from 'docx';
+import { Table, Paragraph, TableCell, TableRow, Document, Packer, TextRun, WidthType } from 'docx';
 import type { Card } from "./card";
 
 function toRow(card: Card): InstanceType<typeof TableRow> {
 	return new TableRow({
 		children: [
 			new TableCell({
-				width: { size: 100 },
+				width: { size: 2409, type: WidthType.DXA },
 				children: [
 					new Paragraph({
 						children: [new TextRun({ text: card.term, bold: true })]
@@ -13,6 +13,7 @@ function toRow(card: Card): InstanceType<typeof TableRow> {
 				]
 			}),
 			new TableCell({
+				width: { size: 7229, type: WidthType.DXA },
 				children: [
 					new Paragraph({
 						text: card.definition
@@ -27,7 +28,9 @@ export default async function MakeDoc(cards: Card[]): Promise<Blob> {
 	let rowArray: TableRow[] = cards.map((card) => toRow(card));
 
 	const table = new Table({
-		rows: rowArray
+		rows: rowArray,
+		width: { size: 0, type: WidthType.AUTO },
+		columnWidths: [2409, 7229]
 	});
 
 	const doc = new Document({
