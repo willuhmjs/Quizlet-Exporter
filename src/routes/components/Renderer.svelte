71b9f2d61b7bfdcf50bcd1023ae8d612169e53f9
William Faircloth <willuhmjs@gmail.com>
2022-12-09 16:42:04 +0000

    format

--- src/routes/components/Renderer.svelte
+++ src/routes/components/Renderer.svelte
@@ -12,35 +12,35 @@
 </script>
 
 <div class="container">
-    <div class="leftSide">
-        <h1>Quizlet to DOCX</h1>
-        <p>Easily save a Quizlet study set as a Word document.</p>
-        <label for="html">Upload HTML file:</label>
-        <input accept=".html" bind:files name="html" type="file" />
+	<div class="leftSide">
+		<h1>Quizlet to DOCX</h1>
+		<p>Easily save a Quizlet study set as a Word document.</p>
+		<label for="html">Upload HTML file:</label>
+		<input accept=".html" bind:files name="html" type="file" />
 
-        <div>
-            {#if files}
-                {#each Array.from(files) as file}
-                    {#await createDocx(file)}
-                        <p>Loading...</p>
-                    {:then}
-                        <pre>File has been downloaded successfully.</pre>
-                    {:catch error}
-                        <pre>{error}</pre>
-                    {/await}
-                {/each}
-            {/if}
-        </div>
-    </div>
-    <div class="rightSide">
-        <h3>How to use:</h3>
-        <ol>
-            <li>Go to the Quizlet study set.</li>
-            <li>Press <kbd>ctrl/cmd + s</kbd> and save the page as "Webpage, HTML Only."</li>
-            <li>Upload the HTML file to the website.</li>
-            <li>Enjoy your newly created Word document!</li>
-        </ol>
-    </div>
+		<div>
+			{#if files}
+				{#each Array.from(files) as file}
+					{#await createDocx(file)}
+						<p>Loading...</p>
+					{:then}
+						<pre>File has been downloaded successfully.</pre>
+					{:catch error}
+						<pre>{error}</pre>
+					{/await}
+				{/each}
+			{/if}
+		</div>
+	</div>
+	<div class="rightSide">
+		<h3>How to use:</h3>
+		<ol>
+			<li>Go to the Quizlet study set.</li>
+			<li>Press <kbd>ctrl/cmd + s</kbd> and save the page as "Webpage, HTML Only."</li>
+			<li>Upload the HTML file to the website.</li>
+			<li>Enjoy your newly created Word document!</li>
+		</ol>
+	</div>
 </div>
 
 <style>
@@ -59,29 +59,29 @@
 	}
 
 	label {
-        display: block;
-        margin-bottom: 10px;
-    }
+		display: block;
+		margin-bottom: 10px;
+	}
 
-    .container {
-        display: flex;
-        justify-content: space-between;
-        margin: 0 auto;
-        max-width: 800px;
-    }
+	.container {
+		display: flex;
+		justify-content: space-between;
+		margin: 0 auto;
+		max-width: 800px;
+	}
 
-    .leftSide {
-        width: 50%;
-    }
+	.leftSide {
+		width: 50%;
+	}
 
-    .rightSide {
-        width: 50%;
-    }
+	.rightSide {
+		width: 50%;
+	}
 
-    kbd {
-        background: #eee;
-        border-radius: 4px;
-        padding: 2px 4px;
-        font-size: 0.8em;
-    }
+	kbd {
+		background: #eee;
+		border-radius: 4px;
+		padding: 2px 4px;
+		font-size: 0.8em;
+	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: "/Quizlet-Exporter"
+			base: '/Quizlet-Exporter'
 		}
 	}
 };
